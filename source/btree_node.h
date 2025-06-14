57c72398fac978b03477a0bddcfb4ff124972b04
Phillip Voyle <phillipvoyle@hotmail.com>
2025-06-15 11:11:22 +1200

    adjust documentation, and sizes to align with 16 bit boundary

--- source/btree_node.h
+++ source/btree_node.h
@@ -16,27 +16,36 @@ class btree_node
     /*
     * Memory layout of a btree node:
     * uint64_t: transaction_id (8 bytes)
-    * uint8_t: is_leaf_node (1 byte)
-    * uint8_t: value_count (1 byte)
+    * uint16_t: flags (2 bytes) (including first bit)
+    * uint16_t: value_count (2 byte)
     * uint16_t: key size (2 bytes)
-    * uint32_t: value size (4 bytes) (this will be a value of 16 to accomodate far_offset_ptr for branch nodes)
+    * uint16_t: value_size (2 bytes)
+    * 
     * array of data
     * each is a key-value pair
     */
 
+    // note the keys and values are not large enough to hold sizes of large data, but in any realistic large data case
+    // we'll want to simply add references to the real data location here and for that, a far ptr is probably fine
+
+    // in the case of non leaf nodes, the values will reliably be far pointers so will be 128 bit values i.e a file id and offset
+    // that's hopefully plenty of space to hold realistic data sets for the near future
+
     std::vector<uint8_t> data;
+
+    static const uint8_t is_leaf_bit_mask = 0x1;
 
     static const size_t transaction_id_offset = 0;
     static const size_t transaction_id_size = 8; // big enough?
 
-    static const size_t is_leaf_node_offset = transaction_id_offset + transaction_id_size;
-    static const size_t is_leaf_node_size = 1;
-    static const size_t value_count_offset = is_leaf_node_offset + is_leaf_node_size;
-    static const size_t value_count_size = 1;
-    static const size_t key_size_offset = value_count_offset + value_count_size;
+    static const size_t flags_offset = transaction_id_offset + transaction_id_size;
+    static const size_t flags_size = 2;
+    static const size_t entry_count_offset = flags_offset + flags_size;
+    static const size_t entry_count_size = 2;
+    static const size_t key_size_offset = entry_count_offset + entry_count_size;
     static const size_t key_size_size = 2;
     static const size_t value_size_offset = key_size_offset + key_size_size;
-    static const size_t value_size_size = 4;
+    static const size_t value_size_size = 2;
     static const size_t data_offset = value_size_offset + value_size_size;
 
     static constexpr size_t get_header_size()
@@ -49,7 +58,7 @@ public:
 
     virtual bool is_leaf() const
     {
-        return data[is_leaf_node_offset] != 0;
+        return (data[flags_offset] & is_leaf_bit_mask) != 0;
     }
 
     virtual uint64_t get_transaction_id()
@@ -59,6 +68,13 @@ public:
         return read_uint64(it);
     }
 
+    virtual void set_transaction_id(uint64_t transaction_id)
+    {
+        std::span<uint8_t> transaction_id_span(data.begin() + transaction_id_offset, transaction_id_size);
+        span_iterator it(transaction_id_span);
+        write_uint64(it, transaction_id);
+    }
+
     virtual uint16_t get_key_size()
     {
         std::span<uint8_t> key_size_span(data.begin() + key_size_offset, key_size_size);
@@ -66,9 +82,25 @@ public:
         return read_uint16(key_size_iter);
     }
 
-    virtual uint8_t get_value_count()
+    virtual void set_key_size(uint16_t key_size)
     {
-        return data[1];
+        std::span<uint8_t> key_size_span(data.begin() + key_size_offset, key_size_size);
+        span_iterator key_size_iter(key_size_span);
+        write_uint16(key_size_iter, key_size);
+    }
+
+    virtual uint16_t get_entry_count()
+    {
+        std::span<uint8_t> value_count_span(data.begin() + entry_count_offset, entry_count_size);
+        span_iterator value_count_iter(value_count_span);
+        return read_uint16(value_count_iter);
+    }
+
+    virtual void set_entry_count(uint16_t value_count)
+    {
+        std::span<uint8_t> value_count_span(data.begin() + entry_count_offset, entry_count_size);
+        span_iterator value_count_iter(value_count_span);
+        write_uint16(value_count_iter, value_count);
     }
 
     virtual uint32_t get_value_size()
@@ -76,6 +108,13 @@ public:
         std::span<uint8_t> value_size_span(data.begin() + value_size_offset, value_size_size);
         span_iterator value_size_iter(value_size_span);
         return read_uint16(value_size_iter);
+    }
+
+    virtual void set_value_size(uint32_t value_size)
+    {
+        std::span<uint8_t> value_size_span(data.begin() + value_size_offset, value_size_size);
+        span_iterator value_size_iter(value_size_span);
+        write_uint16(value_size_iter, value_size);
     }
 
     virtual filesize_t calculate_buffer_size()
@@ -182,7 +221,6 @@ public:
         if (!replace)
         {
             data.resize(data_offset + pair_size * (count + 1));
-
             // Move existing data at and after the insert position back by one pair_size
             if (n < count)
             {
@@ -192,10 +230,14 @@ public:
                     (count - n) * pair_size
                 );
             }
+
         }
 
         std::span<uint8_t> destination(data.begin() + offset, pair_size);
         std::copy(entry.begin(), entry.end(), destination);
+
+        set_entry_count(calculate_entry_count_from_buffer_size());
+        return n;
     }
 
     template<Binary_iterator It>
