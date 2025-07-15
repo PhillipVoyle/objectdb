03acd6549cf0156a47d29d7d0ab31dbe3e7d8cdf
Phillip Voyle <phillipvoyle@hotmail.com>
2025-07-15 16:54:43 +1200

    proto repl

--- objectdb/objectdb.cpp
+++ objectdb/objectdb.cpp
@@ -31,26 +31,31 @@ void dump_tree_node(file_cache& cache, far_offset_ptr offset, const std::string&
     for (int i = 0; i < metadata.entry_count; i ++)
     {
         auto key_span = node.get_key_at(i);
-        std::vector<uint8_t> key_bytes{ key_span.begin(), key_span.end() };
+        //std::vector<uint8_t> key_bytes{ key_span.begin(), key_span.end() };
+
 
         auto value_span = node.get_value_at(i);
-        std::vector<uint8_t> value_bytes{ value_span.begin(), value_span.end() };
+        //std::vector<uint8_t> value_bytes{ value_span.begin(), value_span.end() };
 
+        /*
         span_iterator key_it{ key_bytes };
         auto key = read_uint32(key_it);
-
+        */
+        std::string key{ key_span.begin(), key_span.end() };
 
         if (is_leaf)
         {
-            span_iterator value_it{ value_bytes };
-            auto value = read_uint32(value_it);
+            //span_iterator value_it{ value_bytes };
+            //auto value = read_uint32(value_it);
+
+            std::string value{ value_span.begin(), value_span.end() };
             std::cout << padding << "   " << "[" << i << "]" << key << ":" << value << std::endl;
         }
         else
         {
             std::cout << padding << "   " << "[" << i << "]" << key << ":";
 
-            span_iterator it{ value_bytes };
+            span_iterator it{ value_span };
             far_offset_ptr sub_node_offset;
             sub_node_offset.read(it);
 
@@ -88,10 +93,11 @@ void main()
 
     far_offset_ptr initial{ 0, 0 };
 
-    uint32_t key_size = 800;
-    uint32_t value_size = 16;
+    uint32_t key_size = 200;
+    uint32_t value_size = 600;
 
     btree tree{ cache, initial, allocator, key_size, value_size };
+    /*
     dump_tree(cache, tree);
 
     std::vector<uint8_t> entry(key_size + value_size, 0);
@@ -142,4 +148,163 @@ void main()
         it_remove = tree.remove(transaction_id, it_remove);
         dump_tree(cache, tree);
     }
+    */
+
+    transaction_id = allocator.create_transaction();
+
+    btree_iterator it;
+    for (;;)
+    {
+        write_path(it);
+        //dump_tree(cache, tree);
+        std::string line;
+        std::cout << "btree> " << std::flush;
+        if (std::getline(std::cin, line))
+        {
+            std::stringstream ss(line);
+            std::string command;
+            ss >> command;
+
+            if (command == "ins" || command == "insert")
+            {
+                std::string key;
+                std::string value;
+
+                ss >> key >> value;
+                std::vector<uint8_t> entry(key_size + value_size, 0);
+                std::copy_n(key.begin(), std::min(key_size, (uint32_t)key.size()), entry.begin());
+                std::copy_n(value.begin(), std::min(value_size, (uint32_t)value.size()), entry.begin() + key_size);
+
+                it = tree.seek_begin({ entry.begin(), key_size });
+                if (!it.path.empty() && it.path.back().is_found)
+                {
+                    std::cerr << "entry already exists" << std::endl;
+                }
+                else
+                {
+                    it = tree.insert(transaction_id, it, { entry.begin(), key_size }, { entry.begin() + key_size, value_size });
+                }
+            }
+            else if (command == "upd" || command == "update")
+            {
+                std::string key;
+                std::string value;
+
+                ss >> key >> value;
+                std::vector<uint8_t> entry(key_size + value_size, 0);
+                std::copy_n(key.begin(), std::min(key_size, (uint32_t)key.size()), entry.begin());
+                std::copy_n(value.begin(), std::min(value_size, (uint32_t)value.size()), entry.begin() + key_size);
+
+                it = tree.seek_begin({ entry.begin(), key_size });
+                if (it.path.empty() || !it.path.back().is_found)
+                {
+                    std::cerr << "no entry for key" << std::endl;
+                }
+                else
+                {
+                    it = tree.update(transaction_id, it, { entry.begin(), key_size }, { entry.begin() + key_size, value_size });
+                }
+            }
+            else if (command == "ups" || command == "upsert")
+            {
+                std::string key;
+                std::string value;
+
+                ss >> key >> value;
+                std::vector<uint8_t> entry(key_size + value_size, 0);
+                std::copy_n(key.begin(), std::min(key_size, (uint32_t)key.size()), entry.begin());
+                std::copy_n(value.begin(), std::min(value_size, (uint32_t)value.size()), entry.begin() + key_size);
+
+                it = tree.upsert(transaction_id, { entry.begin(), key_size }, { entry.begin() + key_size, value_size });
+            }
+            else if (command == "sek" || command == "seek")
+            {
+                std::string key;
+
+                ss >> key;
+                std::vector<uint8_t> blob(key_size, 0);
+                std::copy_n(key.begin(), std::min(key_size, (uint32_t)key.size()), blob.begin());
+                it = tree.seek_begin(blob);
+
+                if (it.path.empty() || !it.path.back().is_found)
+                {
+                    std::cerr << "no entry at key" << std::endl;
+                }
+                else
+                {
+                    auto entry = tree.get_entry(it);
+                    std::string value(entry.begin() + key_size, entry.end());
+
+                    std::cout << key << "=" << value << std::endl;
+                }
+            }
+            else if (command == "del" || command == "delete")
+            {
+                std::string key;
+
+                ss >> key;
+                std::vector<uint8_t> blob(key_size, 0);
+                std::copy_n(key.begin(), std::min(key_size, (uint32_t)key.size()), blob.begin());
+                it = tree.seek_begin(blob);
+
+                if (it.path.empty() || !it.path.back().is_found)
+                {
+                    std::cerr << "no entry at key" << std::endl;
+                }
+                else
+                {
+                    it = tree.remove(transaction_id, it);
+                }
+            }
+            else if (command == "xit" || command == "exit")
+            {
+                break;
+            }
+            else if (command == "nxt" || command == "next")
+            {
+                it = tree.next(it);
+            }
+            else if (command == "prv" || command == "previous")
+            {
+                it = tree.prev(it);
+            }
+            else if (command == "end")
+            {
+                it = tree.end();
+            }
+            else if (command == "beg" || command == "begin")
+            {
+                it = tree.begin();
+            }
+            else if (command == "dmp" || command == "dump")
+            {
+                dump_tree(cache, tree);
+            }
+            else if (command == "shw" || command == "show")
+            {
+                if (it.path.empty() || !it.path.back().is_found)
+                {
+                    std::cerr << "no entry" << std::endl;
+                }
+                else
+                {
+                    auto entry = tree.get_entry(it);
+                    std::string key{ entry.begin(), entry.begin() + key_size };
+                    std::string value{ entry.begin() + key_size, entry.begin() + key_size + value_size };
+
+                    std::cout << key << "=" << value << std::endl;
+                }
+            }
+            else
+            {
+                std::cerr << "unrecognised command: " << command << std::endl;
+            }
+        }
+        else
+        {
+            break;
+        }
+
+    }
+    std::cout << "bye";
 }
